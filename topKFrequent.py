6ed32efa850c11d590ac43bb3f32232393be3d54
Oussama Amkhou <oamkhou@e1r3p3.1337.ma>
2026-03-11 22:36:04 +0000

    topKFrequent

--- topKFrequent.py
+++ topKFrequent.py
@@ -1,19 +1,18 @@
+# make a dict with key(array item): value(how many times repeated on the array)
+# sort the dict by the values , reversed
+# store the dict keys on a list
+# return the output as a slicing ,exactly the k number of items
+
 class Solution:
     def topKFrequent(self, nums: list[int], k: int) -> list[int]:
         info = {}
-        out = []
 
         for i in nums:
-            counter = 0
-            for j in nums:
-                if i == j:
-                    counter += 1
-            info[i] = counter
+            info[i] = nums.count(i)
 
-        for key, val in info.items():
-            if val >= k:
-                out.append(key)
-        return out
+        info = dict(sorted(info.items(), key=lambda a: a[1], reverse=True))
+        out = [k for k in info.keys()]
+        return out[:k]
 
 
 solution = Solution()
